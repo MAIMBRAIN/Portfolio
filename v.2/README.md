bb48c0d7cbb2d337086b5b6035385b51380bbee3
Avery Stahl <averystahl@hotmail.com>
2021-02-04 09:42:42 -0500

    Previously updated portfolio repo was deleted somehow... merging with original, will be attempting portfolio v3

---
+++ v.2/README.md
@@ -0,0 +1,2 @@
+# Responsive-Portfolio
+ A dynamic portfolio designed to display my projects
---
+++ v.2/assets/js/db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS portfolio;
+
+CREATE DATABASE portfolio;
+
+USE portfolio;
+
+CREATE TABLE projects(
+    name VARCHAR(50) NOT NULL,
+    
+);
