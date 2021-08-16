cb09949df259fec332040d3a00a2787d35c30d7e
Avery Stahl <averystahl@hotmail.com>
2021-08-16 17:25:19 -0400

    made drawer mobile friendly / responsive

--- v.3/client/src/App.js
+++ v.3/client/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Navbar from './Components/Navbar';
+import About from './Components/About/about';
+import Projects from './Components/Projects/projects';
+import Contact from './Components/Contact/contact';
 
 function App() {
   return (
     <div className="App">
     <Router>
-      <Route path='/' component={App}>
-        <Navbar/>
-      </Route>
+      <Navbar/>
+      <Switch>
+        <Route exact from="/" render={props => <About {...props}/>} />
+        <Route exact path="/projects" render={props => <Projects {...props}/>} />
+        <Route exact path="/contact" render={props => <Contact {...props}/>} />
+      </Switch>
     </Router>
     </div>
   );
--- v.3/client/src/Components/Navbar.jsx
+++ v.3/client/src/Components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import Drawer from "@material-ui/core/Drawer";
+import { Hidden } from "@material-ui/core";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import AccountCircle from "@material-ui/icons/AccountCircle";
+import AccountBox from "@material-ui/icons/AccountBoxTwoTone";
 import MenuIcon from "@material-ui/icons/Menu";
-import BuildIcon from '@material-ui/icons/Build';
+import BuildIcon from '@material-ui/icons/BuildTwoTone';
+import BookIcon from '@material-ui/icons/BookTwoTone';
+import ForumIcon from '@material-ui/icons/ForumTwoTone';
 import { makeStyles, Typography, Toolbar, IconButton } from "@material-ui/core";
 import Appbar from "@material-ui/core/AppBar";
 
@@ -16,20 +20,43 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
+  navStyle: {
+    backgroundColor: "#303030"
+  },
   menuButton: {
     marginRight: theme.spacing(2),
+  },
+  drawer: {
+    width: "240px",
+    backgroundColor: "#7A2626"
+  },
+  listStyle: {
+    marginTop: "auto",
+    marginBottom: "auto"
+  },
+  listItems: {
+    paddingTop: "36px",
+    paddingBottom: "36px",
+  },
+  listText: {
+    color: "#ECF4F9"
+  },
+  listIcon: {
+    color: "#ECF4F9"
   },
   title: {
     marginLeft: "auto",
     marginRight: theme.spacing(2),
+    padding: "8px",
+    color: "#ECF4F9"
   },
 }));
 
 // Main export
-function Navbar() {
+function Navbar (props) {
   // Use imported inline styling
   const classes = useStyles();
-  
+
   // Open or close the drawer
   const [drawerOpen, setDrawer] = React.useState(false);
 
@@ -50,23 +77,45 @@ function Navbar() {
   const itemList = [
     {
       text: "About",
-      icon: <AccountCircle/>
+      icon: <AccountBox/>,
+      url: ""
     },
     {
       text: "Projects",
-      icon: <BuildIcon/>
+      icon: <BuildIcon/>,
+      url: ""
     },
     {
-      text: "Blog"
+      text: "Blog",
+      icon: <BookIcon/>,
+      url: ""
     },
     {
-      text: "Contact"
+      text: "Contact",
+      icon: <ForumIcon/>,
+      url: ""
     },
   ]
 
+  const drawer = (
+    <List className={classes.listStyle}>
+      {itemList.map((item, index) => {
+        const { text, icon } = item;
+        return(
+          <ListItem className={classes.listItems} button key={text} >
+            <ListItemIcon className={classes.listIcon}>{icon}</ListItemIcon>
+            <ListItemText className={classes.listText} primary={text} />
+          </ListItem>
+        )
+      })}
+    </List>
+  );
+
   return (
     <div className={classes.root}>
-      <Appbar position="static">
+      {/* MOBILE */}
+      <Hidden smUp implementation="js">
+      <Appbar position="static" className={classes.navStyle}>
         <Toolbar>
           <IconButton
             edge="start"
@@ -76,18 +125,8 @@ function Navbar() {
             onClick={() => toggleDrawer(true)}
           >
             <MenuIcon />
-            <Drawer open={drawerOpen}>
-              <List>
-                {itemList.map((item, index) => {
-                    const { text, icon } = item;
-                    return(
-                      <ListItem button key={text}>
-                        <ListItemIcon>{icon}</ListItemIcon>
-                        <ListItemText primary={text} />
-                      </ListItem>
-                    )
-                })}
-              </List>
+            <Drawer classes={{paper: classes.drawer}} open={drawerOpen} ModalProps={{keepMounted:true}}>
+              {drawer}
             </Drawer>
           </IconButton>
           <Typography variant="h4" className={classes.title}>
@@ -95,8 +134,21 @@ function Navbar() {
           </Typography>
         </Toolbar>
       </Appbar>
+      </Hidden>
+
+      {/* DESKTOP */}
+      <Hidden xsDown implementation="js">
+        <Drawer classes={{paper: classes.drawer}} variant="permanent" anchor="left">
+          {drawer}
+        </Drawer>
+        <Appbar position="static" className={classes.navStyle}>
+          <Typography variant="h4" className={classes.title}>
+            Avery Stahl's Portfolio
+          </Typography>
+        </Appbar>
+      </Hidden>
     </div>
   );
 }
 
-export default Navbar;
+export default withRouter(Navbar);
