ac362a3ddcf8599e9e1b61966907f58b21a2fb94
Avery Stahl <averystahl@hotmail.com>
2021-08-21 01:16:04 -0400

    imported old about page

--- v.3/client/src/Components/About/about.jsx
+++ v.3/client/src/Components/About/about.jsx
@@ -12,7 +12,21 @@ function About () {
     const classes = useStyles();
     return(
         <div >
-            <p>This is the about page</p>
+            <br></br>
+                <h1 > Avery Stahl </h1>
+                <h3 >Full Stack Web Developer</h3>
+                {/* <img className="img-fluid mx-auto d-block portrait" srcSet={MyPic} alt="Portrait of Avery Stahl" /> */}
+                <br></br>
+                <p>
+                    My name is Avery Stahl and I am a Full Stack Web Developer.
+                    I acquired my skills through the University of Central Florida Coding Bootcamp
+                    and intend to continue with my education through self - taught research and experimentation
+                    by pushing myself with challenging projects, along with keeping up with the growing technologies
+                    and adapting to them. Within the three months in which I attended the coding bootcamp, I have
+                    gained a substantial amount of knowledge and the ability to apply the MERN Stack through small
+                    projects and building this website. 
+                </p>
+                <br></br>
         </div>
     );
 };
