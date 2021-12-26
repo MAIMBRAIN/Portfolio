ca284ca4b199d393f390c02cd97e6016a3bb1ba8
Avery Stahl <averystahl@hotmail.com>
2021-12-26 11:26:44 -0500

    Created an array of objects to be destructured into reusable card component for new additions to the project list.
    Created an array of objects to be destructured into reusable card component for new additions to the project list. I only need to add a new object to projectList[_] to add a new project. The card is already created. Next I need to align the cards and resize them. Also thinking of adding quick scrolling to the next set of project cards.

--- v.3/client/src/Components/Projects/projects.jsx
+++ v.3/client/src/Components/Projects/projects.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Card, CardActions, CardActionArea, CardContent, CardMedia, makeStyles, Typography } from '@material-ui/core';
+import { Button, Card, CardActions, CardActionArea, CardContent, CardMedia, makeStyles, Typography, Grid } from '@material-ui/core';
 
 import MemoryGamePic from "./MemoryGame.jpg";
-
 
 
 const useStyles = makeStyles(theme => ({
@@ -12,46 +11,83 @@ const useStyles = makeStyles(theme => ({
         marginRight: "1rem"
     },
     card: {
-        maxWidth: "40rem",
+        maxWidth: '30rem',
+        marginBottom: '2rem',
+        backgroundColor: '#303030',
     },
-    media: {
-        height: "10rem",
+    cardPic: {
+        height: "20rem",
         // width: "200px",
     },
+    cardText: {
+        color: "#ECF4F9"
+    }
     
 }))
 
 function Projects ({match}) {
     const classes = useStyles();
 
+
+    const projectList = [
+        {
+            title:"Memory Game",
+            image: MemoryGamePic,
+            description: "Test your memory by selecting each Pokémon once! Be careful though, every selection shuffles them around. Good Luck",
+            gitURL: "https://github.com/MAIMBRAIN/Clicky-Game",
+            viewURL: `${match.url + "/memory_game"}`
+        },
+        {
+            title:"RedeFinance",
+            image: "",
+            description: "Having trouble keeping up on your monthly subscriptions and how much you owe? Try out this finance calculator!",
+            gitURL: "https://github.com/MAIMBRAIN/RedeFinance",
+            viewURL: `${match.url + "/redefinance"}`
+        }
+    ];
+
+    const projects = (
+        <div>
+            <Grid container>
+            {projectList.map((item, index) => {
+                const {title, image, description, gitURL, viewURL} = item;
+                return(
+                    <Grid item xs={12}>
+                        <Card className={classes.card}>
+                            <CardActionArea>
+                                <CardMedia
+                                className={classes.cardPic}
+                                image={image}
+                                title={title}
+                                />
+                                <CardContent className={classes.cardText}>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    {title}
+                                </Typography>
+                                <Typography variant="body2" component="p">
+                                    {description}
+                                </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions className={classes.cardText}>
+                                <Button size="small">
+                                    <Link to={{pathname: gitURL}} target="_blank" rel="noreferrer noopener" className={classes.cardText}> View Source Code on GitHub! </Link>
+                                </Button>
+                                <Button size="small">
+                                    <Link to={viewURL} target="_blank" rel="noreferrer noopener" className={classes.cardText}> See More in a New Tab! </Link>
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </Grid>
+                )
+            })}
+            </Grid>
+        </div>
+    );
+
     return(
         <div className={classes.root}>
-            <Card className={classes.card}>
-                <CardActionArea>
-                    <CardMedia
-                    className={classes.media}
-                    image={MemoryGamePic}
-                    title="Memory Game"
-                    />
-                    <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        Memory Game
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        Test your memory by selecting each Pokémon once! Be careful though, every selection shuffles them around. Good Luck!
-                    </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <CardActions>
-                    <Button size="small" color="primary">
-                        <Link to={{pathname: "https://github.com/MAIMBRAIN/Clicky-Game"}} target="_blank"> View Source Code on GitHub! </Link>
-                    </Button>
-                    <Button size="small" color="primary">
-                        <Link to={match.url + "/memory_game"} target="_blank"> See More in a New Tab! </Link>
-                    </Button>
-                </CardActions>
-            </Card>
-
+            {projects}
         </div>
     );
 };
